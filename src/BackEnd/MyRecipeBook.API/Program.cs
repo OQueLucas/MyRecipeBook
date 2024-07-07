79103da9190f8d84c08d794e902258c6a785e702
OQueLucas <l_queiroz@outlook.com.br>
2024-07-07 12:24:19 -0300

    feat: create class to recover logged user

--- src/BackEnd/MyRecipeBook.API/Program.cs
+++ src/BackEnd/MyRecipeBook.API/Program.cs
@@ -2,7 +2,9 @@ using Microsoft.OpenApi.Models;
 using MyRecipeBook.API.Converters;
 using MyRecipeBook.API.Filters;
 using MyRecipeBook.API.Middleware;
+using MyRecipeBook.API.Token;
 using MyRecipeBook.Application;
+using MyRecipeBook.Domain.Security.Tokens;
 using MyRecipeBook.Infrastructure;
 using MyRecipeBook.Infrastructure.Extensions;
 using MyRecipeBook.Infrastructure.Migrations;
@@ -52,8 +54,11 @@ builder.Configuration.GetConnectionString("DatabaseType");
 
 builder.Services.AddApplication(builder.Configuration);
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddScoped<ITokenProvider, HttpContextTokenValue>();
 
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
