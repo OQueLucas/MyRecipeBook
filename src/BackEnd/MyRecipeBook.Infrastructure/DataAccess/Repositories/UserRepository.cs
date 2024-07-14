db29377fd5de2b169d49479737c138f92bdde918
OQueLucas <l_queiroz@outlook.com.br>
2024-07-14 13:42:14 -0300

    feat: implement update user use case, repository and validate

--- src/BackEnd/MyRecipeBook.Infrastructure/DataAccess/Repositories/UserRepository.cs
+++ src/BackEnd/MyRecipeBook.Infrastructure/DataAccess/Repositories/UserRepository.cs
@@ -3,7 +3,7 @@ using MyRecipeBook.Domain.Entities;
 using MyRecipeBook.Domain.Repositories.User;
 
 namespace MyRecipeBook.Infrastructure.DataAccess.Repositories;
-public class UserRepository : IUserWriteOnlyRepository, IUserReadOnlyRepository
+public class UserRepository : IUserWriteOnlyRepository, IUserReadOnlyRepository, IUserUpdateOnlyRepository
 {
     private readonly MyRecipeBookDbContext _dbContext;
 
@@ -22,4 +22,13 @@ public class UserRepository : IUserWriteOnlyRepository, IUserReadOnlyRepository
             .AsNoTracking()
             .FirstOrDefaultAsync(user => user.Active && user.Email.Equals(email) && user.Password.Equals(password));
     }
+
+    public async Task<User> GetById(long id)
+    {
+        return await _dbContext
+            .Users
+            .FirstAsync(user => user.Id == id);
+    }
+
+    public void Update(User user) => _dbContext.Users.Update(user);
 }
--- src/BackEnd/MyRecipeBook.Infrastructure/DependencyInjectionExtension.cs
+++ src/BackEnd/MyRecipeBook.Infrastructure/DependencyInjectionExtension.cs
@@ -68,6 +68,7 @@ public static class DependencyInjectionExtension
 
         services.AddScoped<IUserReadOnlyRepository, UserRepository>();
         services.AddScoped<IUserWriteOnlyRepository, UserRepository>();
+        services.AddScoped<IUserUpdateOnlyRepository, UserRepository>();
     }
 
     private static void AddFluentMigrator_SqlServer(IServiceCollection services, IConfiguration configuration)
