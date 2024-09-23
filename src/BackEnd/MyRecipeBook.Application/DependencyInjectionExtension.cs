513ddbec0dab828ec596e2981eb2c9542639def6
OQueLucas <l_queiroz@outlook.com.br>
2024-09-23 17:21:08 -0300

    feat: implement automapper to register recipe and applying sqids to encode id

--- src/BackEnd/MyRecipeBook.Application/DependencyInjectionExtension.cs
+++ src/BackEnd/MyRecipeBook.Application/DependencyInjectionExtension.cs
@@ -7,21 +7,28 @@ using MyRecipeBook.Application.UseCases.User.ChangePassword;
 using MyRecipeBook.Application.UseCases.User.Profile;
 using MyRecipeBook.Application.UseCases.User.Register;
 using MyRecipeBook.Application.UseCases.User.Update;
+using Sqids;
 
 namespace MyRecipeBook.Application;
 public static class DependencyInjectionExtension
 {
     public static void AddApplication(this IServiceCollection services, IConfiguration configuration)
     {
-        AddAutoMapper(services);
+        AddAutoMapper(services, configuration);
         AddUseCases(services);
     }
 
-    private static void AddAutoMapper(IServiceCollection services)
+    private static void AddAutoMapper(IServiceCollection services, IConfiguration configuration)
     {
+        var sqid = new SqidsEncoder<long>(new()
+        {
+            MinLength = 3,
+            Alphabet = configuration.GetValue<string>("Settings:IdCryptographyAlphabet")!
+        });
+
         services.AddScoped(option => new AutoMapper.MapperConfiguration(options =>
         {
-            options.AddProfile(new AutoMapping());
+            options.AddProfile(new AutoMapping(sqid));
         }).CreateMapper());
     }
 
--- src/BackEnd/MyRecipeBook.Application/Services/AutoMapper/AutoMapping.cs
+++ src/BackEnd/MyRecipeBook.Application/Services/AutoMapper/AutoMapping.cs
@@ -1,12 +1,17 @@
 ﻿using AutoMapper;
+using MyRecipeBook.Communication.Enums;
 using MyRecipeBook.Communication.Requests;
 using MyRecipeBook.Communication.Responses;
+using Sqids;
 
 namespace MyRecipeBook.Application.Services.AutoMapper;
 public class AutoMapping : Profile
 {
-    public AutoMapping()
+    private readonly SqidsEncoder<long> _idEncoder;
+    public AutoMapping(SqidsEncoder<long> idEncoder)
     {
+        _idEncoder = idEncoder;
+
         RequestToDomain();
         DomainToResponse();
     }
@@ -15,10 +20,25 @@ public class AutoMapping : Profile
     {
         CreateMap<RequestRegisterUserJson, Domain.Entities.User>()
             .ForMember(dest => dest.Password, opt => opt.Ignore());
+
+        CreateMap<RequestRecipeJson, Domain.Entities.Recipe>()
+            .ForMember(dest => dest.Instructions, opt => opt.Ignore())
+            .ForMember(dest => dest.Ingredients, opt => opt.MapFrom(source => source.Ingredients.Distinct()))
+            .ForMember(dest => dest.DishTypes, opt => opt.MapFrom(source => source.DishTypes.Distinct()));
+
+        CreateMap<string, Domain.Entities.Ingredient>()
+            .ForMember(dest => dest.Item, opt => opt.MapFrom(source => source));
+
+        CreateMap<DishType, Domain.Entities.DishType>()
+            .ForMember(dest => dest.Type, opt => opt.MapFrom(source => source));
+
+        CreateMap<RequestInstructionJson, Domain.Entities.Instruction>();
     }
 
     private void DomainToResponse()
     {
         CreateMap<Domain.Entities.User, ResponseUserProfileJson>();
+        CreateMap<Domain.Entities.Recipe, ResponseRegisteredRecipeJson>()
+            .ForMember(dest => dest.Id, config => config.MapFrom(source => _idEncoder.Encode(source.Id)));
     }
 }
