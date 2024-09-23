604feb0fa0ec85bcf992851b5bf17f88fe5d84a6
OQueLucas <l_queiroz@outlook.com.br>
2024-09-23 17:43:14 -0300

    test: fix mapper builder with sqids dependency injection

---
+++ tests/CommonTestUtilities/IdEncrypter/IdEncrypterBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Sqids;
+
+namespace CommonTestUtilities.IdEncrypter;
+public class IdEncrypterBuilder
+{
+    public static SqidsEncoder<long> Build()
+    {
+        return new SqidsEncoder<long>(new()
+        {
+            MinLength = 3,
+            Alphabet = "LsmcFyBgQWqSh1wfKADXGiI0Yu93vRU2enMopdjPa87rN4TzZblCJxEH6tkV5O"
+        });
+    }
+}
