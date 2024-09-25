3728c5618a272addf19df4b1dfc864be27c9cb10
OQueLucas <lucasestevam365@gmail.com>
2024-09-25 18:30:27 -0300

    test: implement integration test

--- tests/WebApi.Test/CustomWebApplicationFactory.cs
+++ tests/WebApi.Test/CustomWebApplicationFactory.cs
@@ -1,4 +1,5 @@
 ﻿using CommonTestUtilities.Entities;
+using CommonTestUtilities.IdEncrypter;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
@@ -46,6 +47,7 @@ namespace WebApi.Test
         public string GetName() => _user.Name;
         public Guid GetUserIdentifier() => _user.UserIdentifier;
 
+        public string GetRecipeId() => IdEncrypterBuilder.Build().Encode(_recipe.Id);
         public string GetRecipeTitle() => _recipe.Title;
         public Difficulty GetRecipeDifficulty() => _recipe.Difficulty!.Value;
         public CookingTime GetRecipeCookingTime() => _recipe.CookingTime!.Value;
