e3bd1bee3574ef3488b1bc5eff2b9818bf99c6e3
OQueLucas <lucasestevam365@gmail.com>
2024-09-26 18:22:24 -0300

    fix: move unit of work from data access repository to data access root

--- src/BackEnd/MyRecipeBook.Infrastructure/DataAccess/UnitOfWork.cs
+++ src/BackEnd/MyRecipeBook.Infrastructure/DataAccess/UnitOfWork.cs
@@ -1,6 +1,6 @@
 ﻿using MyRecipeBook.Domain.Repositories;
 
-namespace MyRecipeBook.Infrastructure.DataAccess.Repositories;
+namespace MyRecipeBook.Infrastructure.DataAccess;
 internal class UnitOfWork : IUnitOfWork
 {
     private readonly MyRecipeBookDbContext _dbContext;
