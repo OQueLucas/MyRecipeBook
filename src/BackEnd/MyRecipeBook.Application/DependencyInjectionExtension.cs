8c00dd0f281f76cb3e4297c6c3705a8e2a35157c
OQueLucas <lucasestevam365@gmail.com>
2024-09-26 13:32:54 -0300

    feat: implement delete use case and include dependency injection

--- src/BackEnd/MyRecipeBook.Application/DependencyInjectionExtension.cs
+++ src/BackEnd/MyRecipeBook.Application/DependencyInjectionExtension.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using MyRecipeBook.Application.Services.AutoMapper;
 using MyRecipeBook.Application.UseCases.Login.DoLogin;
+using MyRecipeBook.Application.UseCases.Recipe.Delete;
 using MyRecipeBook.Application.UseCases.Recipe.Filter;
 using MyRecipeBook.Application.UseCases.Recipe.GetById;
 using MyRecipeBook.Application.UseCases.Recipe.Register;
@@ -52,5 +53,6 @@ public static class DependencyInjectionExtension
         services.AddScoped<IRegisterRecipeUseCase, RegisterRecipeUseCase>();
         services.AddScoped<IFilterRecipeUseCase, FilterRecipeUseCase>();
         services.AddScoped<IGetRecipeByIdUseCase, GetRecipeByIdUseCase>();
+        services.AddScoped<IDeleteRecipeUseCase, DeleteRecipeUseCase>();
     }
 }
