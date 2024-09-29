4e81321cd859f794c5a18fd56322e093e8eb9074
OQueLucas <lucasestevam365@gmail.com>
2024-09-29 11:20:47 -0300

    feat: create connection to chatGPT

---
+++ src/BackEnd/MyRecipeBook.Domain/Dtos/GeneratedRecipeDto.cs
@@ -0,0 +1,10 @@
+﻿using MyRecipeBook.Domain.Enums;
+
+namespace MyRecipeBook.Domain.Dtos;
+public class GeneratedRecipeDto
+{
+    public string Title { get; set; } = string.Empty;
+    public IList<string> Ingredients { get; set; } = [];
+    public IList<GeneratedInstructionDto> Instructions { get; set; } = [];
+    public CookingTime CookingTime { get; set; }
+}
