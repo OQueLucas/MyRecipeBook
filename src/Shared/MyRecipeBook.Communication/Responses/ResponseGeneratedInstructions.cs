993f1f6a9c55afc99210904447fe81b1d022ac82
OQueLucas <lucasestevam365@gmail.com>
2024-09-29 11:25:23 -0300

    feat: implement generate recipe use case and endpoint

---
+++ src/Shared/MyRecipeBook.Communication/Responses/ResponseGeneratedInstructions.cs
@@ -0,0 +1,7 @@
+﻿namespace MyRecipeBook.Communication.Responses;
+
+public class ResponseGeneratedInstructions
+{
+    public int Step { get; set; }
+    public string Text { get; set; } = string.Empty;
+}
