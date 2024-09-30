12e10d82ea390dbf96bda0f6a623955a044bc09e
OQueLucas <lucasestevam365@gmail.com>
2024-09-30 16:23:34 -0300

    refactor: refactoring register and addupdate image to remove duplicated code

---
+++ src/BackEnd/MyRecipeBook.Application/Extensions/StreamImageExtensions.cs
@@ -0,0 +1,24 @@
+﻿using FileTypeChecker.Extensions;
+using FileTypeChecker.Types;
+
+namespace MyRecipeBook.Application.Extensions;
+public static class StreamImageExtensions
+{
+    public static (bool isValidImage, string extension) ValidateAndGetImageExtensions(this Stream stream)
+    {
+        var result = (false, string.Empty);
+        if (stream.Is<PortableNetworkGraphic>())
+            result = (true, NomalizeExtension(PortableNetworkGraphic.TypeExtension));
+        else if (stream.Is<JointPhotographicExpertsGroup>())
+            result = (true, NomalizeExtension(JointPhotographicExpertsGroup.TypeExtension));
+
+        stream.Position = 0;
+
+        return result;
+    }
+
+    private static string NomalizeExtension(string extension)
+    {
+        return extension.StartsWith('.') ? extension : $".{extension}";
+    }
+}
